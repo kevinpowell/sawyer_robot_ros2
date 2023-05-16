ccadb78b3668a62f00c1fe0b2f84961dca65148f
Paul Gesel <paulgesel@gmail.com>
2023-05-16 16:28:07 -0400

    add base moveit config

---
+++ sawyer_moveit_config/launch/moveit.launch.py
@@ -0,0 +1,72 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("sawyer_description"),
+                    "urdf",
+                    "full_sawyer.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_description_semantic_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("sawyer_moveit_config"),
+                    "srdf",
+                    "sawyer_base.srdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description_semantic = {"robot_description_semantic": robot_description_semantic_content}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("sawyer_description"), "rviz", "view_robot.rviz"]
+    )
+
+    # joint_state_publisher_node = Node(
+    #     package="joint_state_publisher",
+    #     executable="joint_state_publisher",
+    # )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+    #
+    nodes_to_start = [
+        # joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(nodes_to_start)
+
+
+generate_launch_description()
